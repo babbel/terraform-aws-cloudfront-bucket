7cf813a3c6fe62171bb8cf31fbaf5850c65725e3
André Wendt <awendt@babbel.com>
2022-04-05 11:23:39 +0200

    Use valid bucket names in tests

--- _test/main.tf
+++ _test/main.tf
@@ -1,13 +1,13 @@
 module "s3-bucket-with-cloudfront" {
   source = "./.."
 
-  bucket_name = ""
+  bucket_name = "example-with-default-cert"
 }
 
 module "s3-bucket-with-cloudfront-with-custom-cert" {
   source = "./.."
 
-  bucket_name = ""
+  bucket_name = "example-with-custom-cert"
 
   acm_certificate = {
     arn = "arn:aws:acm:eu-west-1:123456789012:certificate/12345678-1234-1234-1234-123456789012"
