b19cd25aee0a59fd7c3d9f3369e1ffc167e46d5b
André Wendt <awendt@babbel.com>
2022-04-14 09:43:45 +0200

    Remove ignore_changes block, issue seems resolved now

--- bucket.tf
+++ bucket.tf
@@ -20,27 +20,4 @@ data "aws_iam_policy_document" "bucket_policy" {
 resource "aws_s3_bucket_policy" "this" {
   bucket = aws_s3_bucket.this.bucket
   policy = data.aws_iam_policy_document.bucket_policy.json
-
-  lifecycle {
-    ignore_changes = [
-      # When setting a "CanonicalUser" in an S3 bucket policy,
-      # S3 changes the policy into something like
-      # "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity ...",
-      # sometimes with spaces and sometimes with underscores as separators after
-      # "user/".
-      #
-      # We also cannot set that IAM user directly, because we cannot know whether
-      # a bucket accepts an IAM user with spaces or with underscores.
-      #
-      # https://github.com/terraform-providers/terraform-provider-aws/issues/10158
-      #
-      # However, we can always set the documented way using "CanonicalUser",
-      # even if S3 changes value into the IAM user later on.
-      #
-      # We just need to ignore changes on the policy when refreshing
-      # the Terraform state from the S3 API.
-      #
-      policy,
-    ]
-  }
 }
