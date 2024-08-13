b499f15b9cc9921f14d2f247dd76914790bc2ddb
Fionn Masuhr <github.com@fionn.io>
2024-08-13 09:42:01 +0200

    Fix dynamic custom_error_response

--- cloudfront.tf
+++ cloudfront.tf
@@ -70,10 +70,10 @@ resource "aws_cloudfront_distribution" "this" {
     for_each = var.custom_error_response
 
     content {
-      error_caching_min_ttl = custom_error_response.error_caching_min_ttl
-      error_code            = custom_error_response.error_code
-      response_code         = custom_error_response.response_code
-      response_page_path    = custom_error_response.response_page_path
+      error_caching_min_ttl = custom_error_response.value.error_caching_min_ttl
+      error_code            = custom_error_response.value.error_code
+      response_code         = custom_error_response.value.response_code
+      response_page_path    = custom_error_response.value.response_page_path
     }
   }
 
