50fdb91652af08dd691b34dd7d9fa3df9cc51d9a
Jan Sebastian Siwy <github@jansiwy.de>
2024-09-21 20:15:44 +0200

    Update main.tf

--- _test/main.tf
+++ _test/main.tf
@@ -5,13 +5,13 @@ provider "aws" {
 module "s3-bucket-with-cloudfront" {
   source = "./.."
 
-  bucket_name = "example-with-default-cert"
+  s3_bucket_name = "example-with-default-cert"
 }
 
 module "s3-bucket-with-cloudfront-with-custom-cert" {
   source = "./.."
 
-  bucket_name = "example-with-custom-cert"
+  s3_bucket_name = "example-with-custom-cert"
 
   acm_certificate = {
     arn = "arn:aws:acm:eu-west-1:123456789012:certificate/12345678-1234-1234-1234-123456789012"
